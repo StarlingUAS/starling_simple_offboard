44a5b28cc32af8d60ce70fe37e8938de29054af6
Mickey Li <mickeyhli@outlook.com>
2021-09-28 10:52:41 +0100

    Updated run.sh to ensure that environment variables are registered

--- run.sh
+++ run.sh
@@ -1,3 +1,30 @@
-source /etc/starling/vehicle.config
-export VEHICLE_MAVLINK_SYSID
+#!/bin/bash
+
+HAS_VEHICLE_CONFIG=false
+
+# If we have a vehicle.config file, assume we are running on a real vehicle
+if [ -f "/etc/starling/vehicle.config" ]; then
+    echo "Found vehicle.config"
+    HAS_VEHICLE_CONFIG=true
+    # Source VEHICLE_MAVLINK_SYSID, VEHICLE_NAME, VEHICLE_FCU_URL and VEHICLE_FIRMWARE
+    source /etc/starling/vehicle.config
+fi
+
+if [ ! -v $VEHICLE_NAMESPACE ]; then
+    # If set Ensure VEHICLE_NAMESPACE is a valid topic name
+    # Replace all '-' with '_'
+    export VEHICLE_NAMESPACE=${VEHICLE_NAMESPACE//-/_}
+    echo "VEHICLE_NAMESPACE setting to $VEHICLE_NAMESPACE"
+else
+    echo "VEHICLE_NAMESPACE not set, default to mavros_bridge.launch.xml default"
+fi
+
+if [ ! -v $VEHICLE_MAVLINK_SYSID]; then
+    export VEHICLE_MAVLINK_SYSID=$VEHICLE_MAVLINK_SYSID
+    echo "VEHICLE_MAVLINK_SYSID setting to $VEHICLE_MAVLINK_SYSID"
+else
+    export VEHICLE_MAVLINK_SYSID=1
+    echo "VEHICLE_MAVLINK_SYSID not set, default to 1"
+fi
+
 ros2 launch simple_offboard starling_simple_offboard.launch.xml
